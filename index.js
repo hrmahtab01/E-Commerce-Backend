676a9349640c20f0c5fee94a82febcfff4639447
Md Mahtab Ali <161712272+hrmahtab01@users.noreply.github.com>
2024-12-14 01:46:31 +0600

    some update in auth

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require("express");
 const app = express();
 const env = require("dotenv").config();
 const cors = require("cors");
-const router  = require("./Router");
+const router = require("./Router");
+const dbconnect = require("./dbConnect/db.config");
 
 app.use(cors());
 app.use(express.json());
-app.use(router)
+app.use(router);
 
+const port = process.env.port;
 
-
-app.listen(process.env.port, () => {
-  console.log("server is running on port " + process.env.port);
+app.listen(port , () => {
+  dbconnect();
+  console.log("server is running on port " + process.env.port );
 });
