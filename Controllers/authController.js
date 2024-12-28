c61339b9320ccbe440f804ad8aaf12ceb3c9371e
Md Mahtab Ali <161712272+hrmahtab01@users.noreply.github.com>
2024-12-28 15:49:47 +0600

    login part done

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,20 +1,46 @@
+const emailValidationCheck = require("../helpers/validateEmail");
 const userModel = require("../Model/userModel");
 const user = require("../Model/userModel");
 const bcrypt = require("bcrypt");
 
 async function registetionController(req, res) {
   const { name, email, password } = req.body;
-  await bcrypt.hash(password, 10, function (err, hash) {
+  if (!name || !email || !password) {
+    return res.status(400).send({ error: "All fields are required" });
+  }
+  if (!emailValidationCheck(email)) {
+    return res.status(400).send({ error: "Invalid email" });
+  }
+
+  const userExist = await userModel.findOne({ email });
+  if (userExist) {
+    return res.status(404).send({ error: "User already exist" });
+  }
+  bcrypt.hash(password, 10, async function (err, hash) {
     try {
       let user = new userModel({ name, email, password: hash });
-      user.save();
+      await user.save();
       res.status(201).send(user);
-    } catch (error) {}
+    } catch (error) {
+      return res.status(404).send(error);
+    }
   });
 }
 
-function LoginController(req, res) {
-  res.status(200).send("LoginController");
+async function LoginController(req, res) {
+  const {email , password} = req.body
+  const exituser = await userModel.findOne({email})
+  if (exituser) {
+    bcrypt.compare(password, exituser.password).then(function(result) {
+    if (result) {
+      res.status(200).send({message:"login successfully" , user:exituser  })
+    }else{
+      res.status(400).send({error :"invalid credentials"})
+    }
+  });
+  }else{
+    res.status(400).send({error :"invalid credentials"})
+  }
 }
 
 module.exports = { registetionController, LoginController };
