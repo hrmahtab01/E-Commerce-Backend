8a37bf82dc5a5dddf7fe5a5ba843ff51b180435d
Md Mahtab Ali <161712272+hrmahtab01@users.noreply.github.com>
2025-01-05 01:36:59 +0600

    some update in auth

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,7 +1,6 @@
 const e = require("express");
 const emailValidationCheck = require("../helpers/validateEmail");
 const userModel = require("../Model/userModel");
-const user = require("../Model/userModel");
 const bcrypt = require("bcrypt");
 jwt = require("jsonwebtoken");
 
@@ -9,6 +8,15 @@ async function registetionController(req, res) {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     return res.status(400).send({ error: "All fields are required" });
+  }
+  if (!name) {
+    return res.status(400).send({ error: "name is required" });
+  }
+  if (!email) {
+    return res.status(400).send({ error: "email is required" });
+  }
+  if (!password) {
+    return res.status(400).send({ error: "password is required" });
   }
   if (!emailValidationCheck(email)) {
     return res.status(400).send({ error: "Invalid email" });
@@ -19,10 +27,13 @@ async function registetionController(req, res) {
     return res.status(404).send({ error: "User already exist" });
   }
   bcrypt.hash(password, 10, async function (err, hash) {
+    if (err) {
+      return res.status(400).send({ error: err });
+    }
     try {
       let user = new userModel({ name, email, password: hash });
       await user.save();
-      res.status(201).send(user);
+      res.status(201).send({ message: "user registered successfully", user });
     } catch (error) {
       return res.status(404).send(error);
     }
@@ -54,14 +65,14 @@ async function LoginController(req, res) {
             .status(200)
             .send({ message: "login successfully", user: exituser, token });
         } else {
-          res.status(400).send({ error: "invalid credentials" });
+         return res.status(400).send({ error: "invalid credentials" });
         }
       });
     } else {
-      res.status(400).send({ error: "invalid credentials" });
+    return res.status(400).send({ error: "invalid credentials" });
     }
   } catch (error) {
-    res.status(400).send({ message: error });
+   return res.status(400).send({ message: error });
   }
 }
 
