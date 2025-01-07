c4fba1b6276ac0b7b9b843e06d874a5bbb61b8f4
Md Mahtab Ali <161712272+hrmahtab01@users.noreply.github.com>
2025-01-07 15:47:24 +0600

    something add in token verifycation

--- Middleware/getusermiddleware.js
+++ Middleware/getusermiddleware.js
@@ -4,15 +4,20 @@ function getusermiddleware(req, res, next) {
   if (token) {
     jwt.verify(token, process.env.prv_key, function (err, decoded) {
       if (err) {
-        return res.status(401).send({ err: err.message });
-      }
-
-      if (decoded.tokeninfo.role === "admin") {
-        next();
+        return res.status(400).send({ success: false, msg: err.message });
       } else {
-        return res.status(401).send({ err: "credentials unauthorized" });
+        const { role } = decoded.tokeninfo;
+        if (role === "admin") {
+          next();
+        } else {
+          return res
+            .status(401)
+            .send({ success: false, msg: "unauthorized credentials" });
+        }
       }
     });
+  } else {
+    return res.status(404).send({ success: false, msg: "user not found" });
   }
 }
 
