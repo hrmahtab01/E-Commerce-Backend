19a93b9380a8c307b55d85bdd6b51077cb14240c
Md Mahtab Ali <161712272+hrmahtab01@users.noreply.github.com>
2025-01-11 01:58:43 +0600

    update send oto mail

--- helpers/SendEmalil.js
+++ helpers/SendEmalil.js
@@ -3,30 +3,30 @@ const otp = require("./Otpgenerator");
 
 async function Sendemail(email) {
   const transporter = nodemailer.createTransport({
-   service:"gmail", // true for port 465, false for other ports
+    service: "gmail",
     auth: {
       user: process.env.send_otp_email,
       pass: process.env.send_otp_pass,
     },
   });
   const info = await transporter.sendMail({
-    from: process.env.send_otp_email, // sender address
-    to: email, // list of receivers
-    subject: "send otp", // Subject line // plain text body
+    from: process.env.send_otp_email,
+    to: email,
+    subject: "verify your email",
     html: `<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
    <div style="margin:50px auto;width:70%;padding:20px 0">
     <div style="border-bottom:1px solid #eee">
-      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Your Brand</a>
+      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Exclusive</a>
     </div>
     <p style="font-size:1.1em">Hi,</p>
-    <p>Thank you for choosing Your Brand. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p>
+    <p>Please use this OTP to complete your verification. The OTP is valid for the next 10 minutes.</p>
     <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${otp}</h2>
-    <p style="font-size:0.9em;">Regards,<br />Your Brand</p>
+    <p style="font-size:0.9em;">mr mango,<br />Exclusive</p>
     <hr style="border:none;border-top:1px solid #eee" />
     <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-      <p>Your Brand Inc</p>
-      <p>1600 Amphitheatre Parkway</p>
-      <p>California</p>
+      <p> connect with us  </p>
+      <p>Dhaka</p>
+      <p>bangladesh</p>
     </div>
   </div>
 </div>`,
