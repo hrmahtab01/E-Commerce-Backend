7cbbaae5f52a50cafbad51f2fbad0d9fcb1d6802
Md Mahtab Ali <161712272+hrmahtab01@users.noreply.github.com>
2025-01-19 02:47:42 +0600

    image upload done

--- Controllers/categoryController.js
+++ Controllers/categoryController.js
@@ -1,5 +1,8 @@
 function CreatecategoryController(req, res) {
   const { name, description, image } = req.body;
+  console.log(req.file);
+  
+
   res.send(req.body);
 }
 
--- Router/api/Category.js
+++ Router/api/Category.js
@@ -1,8 +1,30 @@
-  const express = require("express");
-const { CreatecategoryController } = require("../../Controllers/categoryController");
-  const router = express.Router();
+const express = require("express");
+const {
+  CreatecategoryController,
+} = require("../../Controllers/categoryController");
+const multer = require("multer");
+const router = express.Router();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniquefilename = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    console.log();
+    const extention = file.originalname.split(".");
+    const extentondata = extention[1];
 
+    cb(null, file.fieldname + "-" + `${uniquefilename}.${extentondata}`);
+  },
+});
 
-  router.post("/createcategory", CreatecategoryController)
+const upload = multer({ storage: storage });
+const path = require("path");
 
-  module.exports = router
+router.post(
+  "/createcategory",
+  upload.single("image"),
+  CreatecategoryController
+);
+
+module.exports = router;
